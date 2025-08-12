754503b51e56c3347fe82845ec8df442282938f5
LEAKEY RAKESH <kenikomark@gmail.com>
2025-08-12 08:38:37 +0300

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -3,7 +3,7 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = "https://nwxbmumzkrmsxvjhmgdl.supabase.co";
+const SUPABASE_URL = "https://wiatrnfvzuqcmcfdnbuw.supabase.co";
 const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im53eGJtdW16a3Jtc3h2amhtZ2RsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUwNDMwMDksImV4cCI6MjA2MDYxOTAwOX0.QM32D_wKwDqkyIiPA95I3hiTWw6-CdRuvZaq0d-ldpU";
 
 // Import the supabase client like this:
